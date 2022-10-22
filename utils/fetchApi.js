531fd604c18a9549ecfdb66e076235db52b8431a
Marius Gavrilescu <mgavrilescu@gmail.com>
2022-10-22 19:24:42 +0300

    env changed

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -7,7 +7,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async (url)=>{
 const {data} = await axios.get(url, {
     headers: {
-        'X-RapidAPI-Key': 'd317c038f9msh028cf537136977fp16b626jsndbd9ee496955',
+        'X-RapidAPI-Key': process.env.RapidAPI,
         'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
       }
 })
